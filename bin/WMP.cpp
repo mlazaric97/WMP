bfeacb4b33d88b2a6a9109b7901e80a61756d0f5
Matthew Juan Lazaric <mlazaric@sn-rfe2.lanl.gov>
2023-05-24 14:34:07 -0600

    can read in '.h5' file

--- bin/WMP.cpp
+++ bin/WMP.cpp
@@ -1,18 +1,14 @@
 #include "WMP.hpp"
 
 using namespace H5;
-const H5std_string FILE_NAME("019039.h5");
-const H5std_string DATASET_NAME("DATA");
+const std::string& FILE_NAME("019039.h5");
+const std::string& DATASET_NAME("DATA");
 
 
 
 int main(void)
 {
-	Exception::dontPrint();
-
-	H5File file(FILE_NAME, H5F_ACC_RDONLY);
-	DataSet dataset = file.openDataSet(DATASET_NAME);
-	H5T_class_t type_class = dataset.getTypeClass();
+	WMP::test_read(FILE_NAME);
 
 	return 0; 
 }
--- src/WMP/reader/h5reader.hpp
+++ src/WMP/reader/h5reader.hpp
@@ -6,7 +6,7 @@ h5fileptr test_read(const std::string& filename)
 {
 	std::cout << H5F_ACC_RDONLY << std::endl; 
 	H5::Exception::dontPrint();
-	H5::H5File * f = 0;
+	H5::H5File* f = 0;
 	try {
 		f = new H5::H5File(filename.c_str(),H5F_ACC_RDONLY);	
 	} catch(const H5::FileIException&) {
