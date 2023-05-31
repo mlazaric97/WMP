5ca3117749499419b8ce17fa69e596360332a3d2
Matthew Juan Lazaric <mlazaric@unm.edu>
2023-05-31 16:45:14 -0600

    end of Wed commit, reading hdf5 correctly

--- bin/WMP.cpp
+++ bin/WMP.cpp
@@ -1,40 +1,44 @@
 #include "WMP.hpp"
-
 using namespace H5;
-<<<<<<< HEAD
-const std::string& FILE_NAME("019039.h5");
-const std::string& DATASET_NAME("DATA");
-=======
+
 const H5std_string FILE_NAME("019039.h5");
 const H5std_string BASE_GROUP("/");
 const H5std_string ISO_GROUP("K39");
 const H5std_string DATASET_NAME("E_max");
->>>>>>> b0141ad (tuesday commits, hdf5 dataset reader fixed, moving functionality of WMP.cpp to header file)
 
 
 
 int main(void)
 {
-<<<<<<< HEAD
-	WMP::h5fileptr f = WMP::test_read(FILE_NAME);
-	WMP::test_find(f);
-=======
-	Exception::dontPrint();
+	H5File file = WMP::openhdf(FILE_NAME);
+	Group  isogrp = WMP::open_isogroup(ISO_GROUP,file); 	
+	WMP::get_E_max(isogrp); 	
+	
 
-	H5File file(FILE_NAME, H5F_ACC_RDONLY);
+
 	std::cout << "here" << std::endl; 
-
-	Group bg = file.openGroup(BASE_GROUP);
-	Group ig = bg.openGroup(ISO_GROUP);
-	std::cout << "opened / group" << std::endl; 
-	DataSet dataset = ig.openDataSet(DATASET_NAME);
 	
-	std::cout << "here 2" << std::endl; 
-
-	H5T_class_t type_class = dataset.getTypeClass();
 	
-	std::cout << "here 3" << std::endl; 
+	
+/*
+	WMP::h5fileptr data_file = WMP::openhdf(FILE_NAME);
+	Group g1 = data_file->openGroup(BASE_GROUP);
+	Group g2 = g1.openGroup(ISO_GROUP);
+	DataSet emax = g2.openDataSet(DATASET_NAME);
 
+
+
+
+	WMP::h5fileptr h5file = WMP::openhdf(FILE_NAME);
+	WMP::h5grpptr  isogrp = WMP::get_iso_grp(ISO_GROUP,h5file); 
+	std::cout << "Made it here" << std::endl; 
+	WMP::get_emax(DATASET_NAME,isogrp);
+
+*/
+
+//	WMP::h5grpptr  iso_grp = WMP::get_iso_grp(ISO_GROUP,data_file);
+//	DataSet emax = iso_grp->openDataSet(DATASET_NAME); 
+
+	std::cout << "PROGRAM FINISHED WITHOUT ERRORS\nGOODBYE" << std::endl; 
 	return 0; 
->>>>>>> b0141ad (tuesday commits, hdf5 dataset reader fixed, moving functionality of WMP.cpp to header file)
 }
--- src/WMP/reader/h5reader.hpp
+++ src/WMP/reader/h5reader.hpp
@@ -1,42 +1,140 @@
+typedef std::shared_ptr<H5::Group>  h5fileptr; 
+typedef std::shared_ptr<H5::Group>  h5grpptr; 
+//typedef std::shared_ptr<H5::DataSet> h5dsptr; 
 
-typedef std::shared_ptr<H5::H5File> h5fileptr;
 
-h5fileptr openhdf(const std::string& filename)
-{
-	std::cout << H5F_ACC_RDONLY << std::endl; 
+// opens and returns a pointer to and hdf5 file denoted by FILENAME; 
+H5::H5File openhdf(const H5std_string FILENAME)
+{	
+	std::cout << "Opening data file: " << FILENAME << std::endl; 
 	H5::Exception::dontPrint();
-	H5::H5File* f = 0;
-	try {
-		f = new H5::H5File(filename.c_str(),H5F_ACC_RDONLY);	
-	} catch(const H5::FileIException&) {
-		f = new H5::H5File(filename.c_str(), H5F_ACC_TRUNC);
+	
+	H5::H5File f; 
+	try { 
+		f = H5::H5File(FILENAME,H5F_ACC_RDONLY);
+	} catch( const H5::FileIException error) {	
+		std::cout << "ERROR OPENING DATAFILE: " << FILENAME << std::endl;
+		error.printErrorStack(); 
+		exit; 
 	}
+	return f; 
+}
+
+// Opens the "/" group at root of all WMP_library files
+// then opens the isotope group denoted by GROUPNAME and returns a pointer to it 
+H5::Group open_isogroup(const H5std_string GROUPNAME,H5::H5File &file)
+{
+	std::cout << "Opening Group: '/'" << std::endl; 
+	H5::Exception::dontPrint();
+	H5::Group g, isog;
+	try {
+		g = file.openGroup("/"); 
+	} catch( const H5::GroupIException erro) {
+		std::cout << "ERROR OPENING BASE GROUP" << std::endl; 
+		erro.printErrorStack(); 
+		exit; 
+	}
+	std::cout << "Opened Group:" << GROUPNAME << std::endl << "Opening Isotope Group\n";	
+	try {
+		 isog = g.openGroup(GROUPNAME);
+	} catch( const H5::GroupIException error) {
+		std::cout << "ERROR OPENING ISOTOPE GROUP" << std::endl; 
+		error.printErrorStack();
+		exit; 
+	}	
+	std::cout << "Successfully opened Isotope Group: "<< GROUPNAME << std::endl; 
+	
+	return isog; 
+}
+
+void get_E_max(H5::Group &isogroup)
+{
+	H5::Exception::dontPrint();
+
+	H5::DataSet ds;
+	try {	
+		ds = isogroup.openDataSet("E_max"); 
+	} catch(const H5::FileIException error) {
+		std::cout << "ERROR OPENING DATASET: 'E_max'\n";
+		error.printErrorStack();
+		exit; 
+	}
+	std::cout << "Successfully opened DataSet 'E_max' " << std::endl; 
+
+}
+
+
+/*
+
+h5fileptr openhdf(const H5std_string FILE_NAME)
+{
+	H5::H5File *f = 0; 
+	try {
+		f = new H5::H5File(FILE_NAME, H5F_ACC_RDONLY); 
+
+	} catch(const H5::FileIException&) {
+		std::cout << "ERROR OPENING DATAFILE: " << FILE_NAME << std::endl; 
+		exit; 
+	}
+	std::cout << "Succesfully opened data file " << std::endl; 
+
 	return h5fileptr(f); 
 
 }
-
-<<<<<<< HEAD
-std::vector
-=======
-
-
-typedef std::shared_ptr<H5::Group> grp_ptr;
-
-grp_ptr get_iso_grp(const H5::H5std_string ISO_NAME); 
->>>>>>> b0141ad (tuesday commits, hdf5 dataset reader fixed, moving functionality of WMP.cpp to header file)
-
-
-
-
-
-<<<<<<< HEAD
-
-
-void test_find(h5fileptr filename)
+*/
+void closehdf(h5fileptr file) 
 {
-	H5::H5File f = *filename; 
-	std::cout << f.getFileSize() << std::endl;
-	
+	file->close(); 
 }
-=======
->>>>>>> b0141ad (tuesday commits, hdf5 dataset reader fixed, moving functionality of WMP.cpp to header file)
+
+h5grpptr get_iso_grp(const H5std_string ISO_NAME, h5fileptr fileptr)
+{
+	std::cout << "Grabbing Data for Isotope: " << ISO_NAME << std::endl; 	
+	
+	H5::Exception::dontPrint();
+
+	H5::Group g;
+	H5::Group isog; 
+	H5std_string BG_NAME("/");
+	try {   
+		g = fileptr->openGroup(BG_NAME);
+	} catch(const H5::FileIException&) {
+		std::cout << "ERROR: CANNOT OPEN '/' GROUP" << std::endl; 
+	}
+
+	try {
+		isog = g.openGroup(ISO_NAME); 
+	} catch(const H5::FileIException&) {
+		std::cout << "ERROR OPENING ISOTOPE GROUP IN DATA FILE" << std::endl; 
+		exit; 
+	}
+	std::cout << "Successfully opened " << ISO_NAME << " group" << std::endl; 
+	return h5grpptr(&isog); 
+
+}
+
+
+void get_emax(const H5std_string dsname, h5grpptr isotope_group)
+{
+	std::cout << "reading dataset: '" << dsname << "'\n";
+	H5::Group ig = *isotope_group; 	
+
+	H5::DataSet ds; 
+	try{
+		ds = ig.openDataSet(dsname);}
+	catch(const H5::GroupIException error) {
+		std::cout << "ERROR: Cannot open dataset: '"<< dsname <<"' in file" << std::endl; 
+		error.printErrorStack();
+		exit;
+	}
+	
+		
+	std::cout << "here2" << std::endl; 
+
+
+
+
+}
+
+
+
