a10cddb9536903451908bc2e31f0cb850c90f9e5
mlazaric97 <mlazaric@unm.edu>
2023-08-22 09:40:48 -0600

    missed this

--- interface/WMP_mod.F90
+++ interface/WMP_mod.F90
@@ -1,16 +1,20 @@
 module wmp_interface_mod
         ! This was built to create Windowed Multipole cross sections in MCNP
         ! the intention is to have a code that will produce cross sections in C, C++ and Fortran 90
-        use ISO_FORTRAN_ENV, only : REAL64
+!        use ISO_FORTRAN_ENV, only : REAL64
 
         use, intrinsic : iso_c_binding
 
         implicit none
 
-        private
 
-        public
+        public :: WMP
 
+        interface
+                subroutine Ntrn(filename,verb) bind(c, name="Ntrn")
+                import :: c_bool, c_char
+                CHARACTER, intent(in), value :: filename
+                LOGICAL
 
 
 
